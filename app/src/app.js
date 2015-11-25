a35cd8b33a371ca892c489f1c01b3802d78b913c
tmfrnz <timo@dumpark.com>
2015-11-26 09:49:54 +1300

    move print routines to print-store

--- app/src/app.js
+++ app/src/app.js
@@ -9,7 +9,8 @@ var actions         = require("actions"),
     
 //stores
 var RouteStore      = require("stores/route-store"),
-    EntityStore     = require("stores/entity-store") 
+    EntityStore     = require("stores/entity-store"), 
+    PrintStore     = require("stores/print-store") 
 
 //utils
 var loadData        = require('utils/load-data')
@@ -52,6 +53,7 @@ var entityStoreConfig = {
 // initialise stores
 var stores = {
   routes:  new RouteStore({ router: router }),
+  printer:  new PrintStore({  }),
   actions: new EntityStore(_.extend({},entityStoreConfig,{ 
     sheet: 'actions',
     type:'action', 
